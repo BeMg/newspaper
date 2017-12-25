95ef1baa69ca829ca9e6d19e7102f04eab062e25
BeMg <gccbg04538@gmail.com>
2017-12-25 18:28:13 +0800

    add applediary

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import bs4
 import json
 import sqlite3
 import re
+from test import page_scroll_to_bottom
+
 
 def getSoup(url):
     r = requests.get(url)
@@ -12,20 +14,36 @@ def getSoup(url):
 def newsInsert(data):
     conn = sqlite3.connect('news.db')
     c = conn.cursor()
-    c.execute("INSERT INTO news VALUES (?,?,?)", data)
+    c.execute("SELECT * FROM news WHERE url = '%s'" % data[0])
+    check = c.fetchall()
+    if(len(check) > 0):
+        pass
+    else:
+        c.execute("INSERT INTO news VALUES (?,?,?)", data)
     conn.commit()
     conn.close()
 
+def Checkindb(url):
+    conn = sqlite3.connect('news.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM news WHERE url = '%s'" % url)
+    check = c.fetchall()
+    if(len(check) > 0):
+        return True
+    else:  
+        return False
+
 def ettoday():
     def getList():
-        s = getSoup("https://www.ettoday.net/news/news-list.htm")
+        r = page_scroll_to_bottom("https://www.ettoday.net/news/news-list.htm")
+        s = bs4.BeautifulSoup(r, 'lxml')
         ss = s.find(class_="part_list_2").findAll('a')
         url_list = []
         for i in ss:
             url_list.append("https://www.ettoday.net"+i['href'])
         return url_list
     
-    def getMain(url):
+    def getContent(url):
         try:
             s = getSoup(url)
             print(url)
@@ -45,10 +63,48 @@ def ettoday():
     l = getList()
     
     for i in l:
-        data = getMain(i)
-        if data == None:
-            pass
+        if Checkindb(i):
+            print("Already have.")
+            continue
         else:
-            newsInsert(data)    
+            data = getContent(i)
+            if data == None:
+                pass
+            else:
+                newsInsert(data)    
 
-ettoday()
+def applediary():
+    def getList():
+        s = getSoup('https://tw.appledaily.com/new/realtime/')
+        ss = s.find(class_="rtddd slvl").findAll('a')
+        url = []
+        for i in ss:
+            url.append(i['href'])
+        return url
+    def getContent(url):
+        try:
+            print(url)
+            s = getSoup(url)
+            title = s.find(class_="ndArticle_leftColumn").find('h1').text
+            main = s.find(class_="ndArticle_margin").find('p').text
+            print("Success")
+            return (url, title, main)
+        except:
+            print("Fail")
+            return None
+
+    l = getList()
+    
+    for i in l:
+        if Checkindb(i):
+            print("Already have.")
+            continue
+        else:
+            data = getContent(i)
+            if data == None:
+                pass
+            else:
+                newsInsert(data)
+
+
+applediary()
---
+++ test.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+
+
+def page_scroll_to_bottom(url):
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get(url)
+
+    SCROLL_PAUSE_TIME = 0.5
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    return driver.page_source
